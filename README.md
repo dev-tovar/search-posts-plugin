1eb37034080fe6e66497623780c25e9a05c43ae6
yecidtovar <yecidtovar.cos@gmail.com>
2023-07-30 12:45:56 -0500

    Busqueda de entradas

---
+++ README.md
@@ -0,0 +1,26 @@
+# Search Posts Plugin
+
+Search Posts Plugin es un sencillo plugin para WordPress que permite a los usuarios realizar búsquedas de posts publicados utilizando un formulario de búsqueda en el frontend.
+
+## Características
+
+- Formulario de búsqueda con un campo de texto y un botón "Buscar".
+- Personalización de estilos CSS para adaptar el formulario a tu tema de WordPress.
+
+## Instalación
+
+1. Descarga el archivo ZIP del plugin desde [GitHub](https://github.com/dev-tovar/search-posts-plugin) o crea un archivo ZIP con el contenido del plugin.
+2. Ve a la sección de "Plugins" en tu panel de WordPress.
+3. Haz clic en "Añadir nuevo" y luego en "Subir plugin".
+4. Selecciona el archivo ZIP del plugin y haz clic en "Instalar ahora".
+5. Activa el plugin "Search Posts Plugin".
+
+## Uso
+
+Para mostrar el formulario de búsqueda en tus páginas o entradas, utiliza el shortcode `[search_posts]`. Puedes agregarlo directamente en el editor de WordPress.
+
+## Personalización
+
+Puedes personalizar los estilos del formulario de búsqueda editando el archivo "style.css" ubicado en la carpeta del plugin.
+
+¡Gracias por usar Search Posts Plugin!
---
+++ search-posts-plugin.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Plugin Name: Search Posts Plugin
+Description: Plugin que permite buscar posts publicados.
+Version: 1.0
+Author: YECID TOVAR TOVAR
+*/
+
+function search_posts_form()
+{
+    // $form = '<form method="get" action="' . esc_url(home_url('/')) . '">';
+    // $form .= '<input type="text" name="s" placeholder="Buscar posts">';
+    // $form .= '<input type="submit" value="Buscar">';
+    // $form .= '</form>';
+
+
+    // $form = '<form id="search-posts-form" method="get" action="' . esc_url(home_url('/')) . '">';
+    // $form .= '<input type="text" name="s" id="search_keywords" placeholder="Buscar posts">';
+    // $form .= '<input type="submit" value="Buscar">';
+    // $form .= '</form>';
+
+    $form = '<form id="search-posts-form" style="display:flex;" method="get" action="' . esc_url(home_url('/')) . '">';
+    $form .= '<input id="namanyay-search-box" name="s" size="40" type="text" placeholder="Buscar posts"/>';
+    $form .= '<input id="namanyay-search-btn" value="Buscar" type="submit"/>';
+    $form .= '</form>';
+
+
+    return $form;
+
+}
+add_shortcode('search_posts', 'search_posts_form');
+
+function search_posts_plugin_scripts()
+{
+    wp_enqueue_style('search-posts-plugin-style', plugins_url('style.css', __FILE__));
+}
+add_action('wp_enqueue_scripts', 'search_posts_plugin_scripts');
+
+function custom_search_filter($query)
+{
+    if (!is_admin() && $query->is_main_query() && $query->is_search()) {
+        $query->set('post_type', 'post');
+    }
+    return $query;
+}
+add_filter('pre_get_posts', 'custom_search_filter');
